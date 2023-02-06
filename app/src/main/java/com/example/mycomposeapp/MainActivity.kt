80de8693124d74a0a0ffffc6e235a855507dcf4b
Ivan <ivan_d03@mail.ru>
2023-02-06 21:48:07 +0300

    Add counters

--- app/src/main/java/com/example/mycomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/mycomposeapp/MainActivity.kt
@@ -3,18 +3,18 @@ package com.example.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -48,10 +48,16 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 private fun ListItem(name: String, prof: String) {
+    var counter = remember {
+        mutableStateOf(0)
+    }
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(10.dp),
+            .padding(10.dp)
+            .clickable {
+                counter.value++
+            },
         shape = RoundedCornerShape(15.dp),
         elevation = 5.dp
         ) {
@@ -68,7 +74,7 @@ private fun ListItem(name: String, prof: String) {
                 )
 
                 Column(modifier = Modifier.padding(16.dp)) {
-                    Text(text = name)
+                    Text(text = counter.value.toString())
                     Text(text = prof)
                 }
             }

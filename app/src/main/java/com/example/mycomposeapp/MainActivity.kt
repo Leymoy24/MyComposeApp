f6a7d5885b06fd4449539a1d42819ca9bebf559d
Ivan <ivan_d03@mail.ru>
2023-02-06 21:23:36 +0300

    Add personal list

--- app/src/main/java/com/example/mycomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/mycomposeapp/MainActivity.kt
@@ -3,45 +3,73 @@ package com.example.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mycomposeapp.ui.theme.MyComposeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
-                modifier = Modifier
-                    .background(Color.Blue)
-                    .fillMaxWidth().fillMaxHeight(0.5f),) {
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                ListItem(name = "Ivan", prof = "Mobile Developer")
+                ListItem(name = "Ivan", prof = "Mobile Developer")
+                ListItem(name = "Ivan", prof = "Mobile Developer")
+                ListItem(name = "Ivan", prof = "Mobile Developer")
+                ListItem(name = "Ivan", prof = "Mobile Developer")
+                ListItem(name = "Ivan", prof = "Mobile Developer")
+                ListItem(name = "Ivan", prof = "Mobile Developer")
+                ListItem(name = "Ivan", prof = "Mobile Developer")
+                ListItem(name = "Ivan", prof = "Mobile Developer")
+                ListItem(name = "Ivan", prof = "Mobile Developer")
+                ListItem(name = "Ivan", prof = "Mobile Developer")
+                ListItem(name = "Ivan", prof = "Mobile Developer")
+            }
+        }
+    }
+}
+@Composable
+private fun ListItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+        ) {
+        Box() {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(  painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                        contentDescription = "image",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .padding(5.dp)
+                            .size(64.dp)
+                            .clip(CircleShape)
+                            .background(Color.Red)
+                )
 
-
-                Column(
-                    modifier = Modifier.background(Color.Red).fillMaxWidth(0.5f).fillMaxHeight(),
-                    verticalArrangement = Arrangement.SpaceEvenly, // расположение элементов
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "Hello!")
-                    Text(text = "Girona")
-                    Text(text = "Madrid")
-                }
-                Column(
-                    modifier = Modifier.background(Color.Yellow).fillMaxSize(),
-                    verticalArrangement = Arrangement.SpaceEvenly, // расположение элементов
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "Hello!")
-                    Text(text = "Girona")
-                    Text(text = "Madrid")
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(text = name)
+                    Text(text = prof)
                 }
             }
         }
